75335ac1b06dbe47e638119dfe297a92af0f9c7e
Vikas <vikasmishrastudyonly@gmail.com>
2024-04-23 09:13:46 +0530

    Added New restful code

---
+++ restful-web-services/src/main/java/com/learning/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.learning.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloworld(){
+        return "HELLO PEOPLE";
+    }
+
+}
