bca32b76a1b0a38e256c8812d30de73bae013b5b
Vikas <vikasmishrastudyonly@gmail.com>
2024-04-24 15:55:38 +0530

    updated restful code

--- restful-web-services/src/main/java/com/learning/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/learning/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.learning.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,13 @@ public class HelloWorldController {
     @GetMapping("/hellojson")
     public HelloWorldBean HelloWorldBean(){
         return new HelloWorldBean("Hello PEOPLE");
+    }
+
+    //parameter
+    @GetMapping("/hello/{id}")
+    public HelloWorldBean HelloWorldBeanPath(@PathVariable String id){
+        //return new HelloWorldBean("Hello " + id);
+        return new HelloWorldBean(String.format("Hello friend %s", id));
     }
 
 }
